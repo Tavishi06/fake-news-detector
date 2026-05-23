fc8a725f737df52e76d1a83bb2209e8b88e1f556
Tavishi06 <tavishikashyap02@gmail.com>
2026-05-23 10:27:25 +0530

    Updated UI

--- ml/train.py
+++ ml/train.py
@@ -3,27 +3,46 @@ import pickle
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 # Load dataset
-df = pd.read_csv(r"E:\Fake news\ml\FakeNewsNet.csv")
+df = pd.read_csv(r"E:\fake news\dataset\FakeNewsNet.csv")
 
-# Input text
+# Remove empty rows
+df = df.dropna()
+
+# Text
 X = df["title"]
 
 # Labels
 y = df["real"]
 
-# Convert text to vectors
-vectorizer = TfidfVectorizer()
+# Convert text
+vectorizer = TfidfVectorizer(stop_words="english")
 
 X_vectorized = vectorizer.fit_transform(X)
 
-# Train model
-model = LogisticRegression()
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X_vectorized,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 
-model.fit(X_vectorized, y)
+# Model
+model = LogisticRegression(max_iter=1000)
 
-# Save model and vectorizer
+# Train
+model.fit(X_train, y_train)
+
+# Accuracy
+predictions = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, predictions))
+
+# Save
 pickle.dump(model, open("model.pkl", "wb"))
 pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
 
